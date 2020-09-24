8c3237dff3e23d658c33832658360db212bed3bf
SHARMA <cs103115@altiriss-MacBook-Pro.local>
2020-09-24 22:58:01 +0530

    Axiom mobile db upload

---
+++ src/main/java/com/chan/axiom/AxiomHiringApplication.java
@@ -0,0 +1,13 @@
+package com.chan.axiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiomHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiomHiringApplication.class, args);
+	}
+
+}
